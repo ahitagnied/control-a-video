9d39d9f620b42e91f75b4524a5b5f99c69c8ab0f
ahitagnied <ad158@rice.edu>
2025-02-19 20:53:41 -0600

    inflatedconv2d + temporalconv1d -> pseydoconv3d

--- models/resnet.py
+++ models/resnet.py
@@ -1,237 +1,49 @@
 import torch
-from dataclasses import dataclass, field
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
+from functools import partial # fix certain arguments of a function, to create a higher order function
 
-class InflatedConv3d(nn.Conv2d):
+class PseudoConv3D(nn.Conv2d):
     """
-    applies a 2d convolution layer across the frames of a video. each frame is convolved 
-    independently
+    applies a spatial 2d convolutional + 1d temporal convolution for video data processing 
     """
+    def __init__(self, in_channels, out_channels, kernel_size, temporal_kernel_size, **kwargs):
+        # pass arguments onto the parent class to do all the usual setup for a conv layer
+        super().__init__(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            **kwargs
+        )
+
+        # if temporal kernel is not present 
+        if temporal_kernel_size == None: temporal_kernel_size = kernel_size
+
+        # initialise a temporal 1d conv layer
+        self.conv_temporal = (nn.Conv1d(
+            out_channels, 
+            out_channels,
+            kernel_size=temporal_kernel_size,
+            padding=temporal_kernel_size//2
+        ) if kernel_size > 1 else None)
+
+        # if self.conv_temporal is not none, then initialise its weights to identity and bias to 0
+        if self.conv_temporal is not None: 
+            nn.init.dirac_(self.conv_temporal.weight.data)
+            nn.init.zeros_(self.conv_temporal.bias.data)
+        
     def forward(self, x):
-        # extract the number of frames in the video
-        video_length = x.shape[2]
-        # (b c f h w) -> ((b*f) f h w)
-        x = rearrange(x, "b f c h w -> (b f) c h w")
-        # apply 2d convolution
-        x = super().conv2d(x)
-        x = rearrange(x, "(b f) c h w -> b f c h w", f=video_length)
+        b = x.shape[0] # num of batches
+        x = rearrange(x, "b c f h w -> (b f) c h w")
+        x = super().forward(x) # pass through a convoution layer of the parent class
+        x = rearrange(x, "(b f) c h w -> b f c h w") # rearrange to video shape 
+
+        *_, h, w = x.shape # retain values of h and w for rearranging
+        
+        # now do a temporal convolution 
+        x = rearrange(x, "b f c h w -> (b h w) f c")
+        x = self.conv_temporal(x)
+        x = rearrange(x, "(b h w) f c -> b g c h w", h = h, w = w)
+
         return x
-
-class TemporalConv1d(nn.Conv2d):
-    """
-    applies a 1d temporal convolution to video data. each spatial location's time sequence is 
-    convolved independently, and then the original shape is restored
-    """
-    def forward(self, x):
-        b, c, f, h, w = x.shape
-        # rearrange to y for temporal conv; b c f h w -> (b h w) c f
-        y = rearrange(x.clone(), "b c f h w -> (b h w) c f") 
-        y = super().forward(y)
-        # rearrange back to b, c, f, h, w
-        y = rearrange(y, "(b h w) c f -> b c f h w", b=b, h=h, w=w)
-        return y
-
-@dataclass
-class UpDownBlock(nn.Module):
-    """
-    applies upsampling to video data using nearest neighbor interpolation, with an optional 
-    convolution applied afterward
-    """
-    channels: int
-    scale_factor: float
-    use_conv: bool = False
-    use_conv_transpose: bool = False
-    outchannels: int = None
-    padding: int = 1
-    name: str = "conv"
-    conv: nn.Module = field(init=False, default=None)
-
-    def __post_init__(self):
-        nn.Module.__init__(self)
-        self.out_channels = self.out_channels or self.channels
-
-        if self.use_conv_transpose:
-            raise NotImplementedError
-        
-        if self.scale_factor < 1: # downsample: use a conv with stride to reduce spatial dimensions.
-            stride = int(round(1/self.scale_factor)) # note: for a scale factor of 0.5, stride is 2
-            if self.use_conv:
-                self.conv = InflatedConv3d(self.channels, self.out_channels, 3, stride=stride, padding=self.padding)
-            else:
-                raise NotImplementedError
-        else: # upsample: use interpolation, then optionally a conv.
-            if self.use_conv:
-                self.conv = InflatedConv3d(self.channels, self.out_channels, 3, padding=1)
-            else: 
-                self.conv = None
-    
-    def forward(self, hidden_states, output_size=None):
-        # downsampling: directly apply the conv (which has a stride)
-        if self.scale_factor < 1:
-            return self.conv(hidden_states) 
-        # upsampling: use nearest neighbor interpolation, then optional conv.
-        else: 
-            dtype = hidden_states.dtype
-            if dtype == torch.bfloat16:
-                hidden_states = hidden_states.to(torch.float32)
-            elif hidden_states.shape[0] >= 64:
-                hidden_states = hidden_states.contiguous()
-
-            if output_size is None:
-                hidden_states = F.interpolate(
-                    hidden_states, 
-                    scale_factor=[1, self.scale_factor, self.scale_factor],
-                    mode="nearest"
-                )
-            else:
-                hidden_states = F.interpolate(
-                    hidden_states, 
-                    size=output_size,
-                    model="nearest"
-                )
-            
-            if dtype == torch.bfloat16:
-                hidden_states = hidden_states.to(dtype)
-            if self.use_conv and self.conv is not None:
-                hidden_states = self.conv(hidden_states)
-            
-            return hidden_states
-
-@dataclass
-class ResnetBlock3D(nn.Module):
-    """
-    applies a 3d resnet block to a video tensor. this block uses group normalization, inflated 3d 
-    convolutions, and an optional time embedding. the block performs normalization, activation, convolution, 
-    dropout, and a shortcut connection. it supports both default and scale_shift modes for time embedding 
-    normalization.
-    """
-    in_channels: int
-    out_channels: int = None
-    conv_shortcut: bool = False
-    dropout: float = 0.0
-    temb_channels: int = 512
-    groups: int= 32
-    groups_out: int = None
-    pre_norm: bool= True
-    eps: float = 1e-6
-    non_linearity: str = "swish"
-    time_embedding_norm: str = "default"
-    output_scale_factor: float = 1.0
-    use_in_shortcut: bool = None
-
-    # the following is initialised by __post_init__
-    norm1: nn.Module = field(init=False)
-    conv1: nn.Module = field(init=False)
-    time_emb_proj: nn.Module = field(init=False)
-    norm2: nn.Module = field(init=False)
-    dropout_layer: nn.Module = field(init=False)
-    conv2: nn.Module = field(init=False)
-    nonlinearity_fn: callable = field(init=False)
-    conv_shortcut_layer: nn.Module = field(init=False, default=None)
-
-    def __post_init__(self):
-        nn.Module.__init__(self)
-
-        # if out_channels is not provided, use in_channels
-        if self.out_channels == None:
-            self.out_channels = self.in_channels
-
-        # if groups_out is not provided, use groups
-        if self.groups_out == None: 
-            self.groups_out = self.groups
-        
-        # as in the original code, force prenorm to be true
-        self.pre_norm = True
-
-        # first normalisation
-        self.norm1 = nn.GroupNorm(num_groups=self.groups, num_channels=self.in_channels, eps=self.eps, affine=True)
-        # first convolution layer: kernel_size=3, stride=1, padding=1
-        self.conv1 = InflatedConv3d(in_channels=self.in_channels, out_channels=self.out_channels, kernel_size=3, stride=1, padding=1)
-
-        # time embedding projection layer (if temb_channels is provided)
-        if self.temb_channels is not None:
-            if self.time_embedding_norm == "default":
-                time_emb_proj_out_channels = self.out_channels
-            elif self.time_embedding_norm == "scale_shift":
-                time_emb_proj_out_channels = self.out_channels * 2
-            else: 
-                raise ValueError(f"unknown time_embedding_norm: {self.time_embedding_norm}")
-            self.time_emb_proj = nn.Linear(self.temb_channels, time_emb_proj_out_channels)
-        else: 
-            self.time_emb_proj = None
-
-        # second normalisation layer
-        self.norm2 = nn.GroupNorm(num_groups=self.groups_out, num_channels=self.out_channels, eps=self.eps, affine=True)
-        # dropout layer
-        self.dropout_layer = nn.Dropout(self.dropout)
-        # second convolution layer: kernel_size=3, stride=1, padding=1
-        self.conv2 = InflatedConv3d(self.out_channels, self.out_channels, kernel_size=3, stride=1, padding=1)
-
-        # define the non-linearity
-        if self.non_linearity == "swish":
-            self.nonlinearity_fn = lambda x: F.silu(x)
-        elif self.non_linearity == "mish":
-            self.nonlinearity_fn = Mish()
-        elif self.non_linearity == "silu":
-            self.nonlinearity = nn.SiLU()
-        else:
-            raise ValueError(f"unknown non_linearity: {self.non_linearity}")
-        
-        ## CODE CORRECTION: CHECK!!
-        if self.use_in_shortcut is None:
-            self.use_in_shortcut = (self.in_channels != self.out_channels)
-        self.conv_shortcut = None
-        if self.use_in_shortcut:
-            self.conv_shortcut = InflatedConv3d(self.in_channels, self.out_channels, kernel_size=1, stride=1, padding=0)
-        
-    def forward(self, input_tensor, temb):
-        hidden_states = input_tensor
-
-        # apply first norm and non-linearity
-        hidden_states = self.norm1(hidden_states)
-        hidden_states = self.nonlinearity_fn(hidden_states)
-
-        # apply first convolution
-        hidden_states = self.conv1(hidden_states)
-
-        # process time embedding if provided
-        if temb is not None:
-            temb = self.nonlinearity_fn(temb)
-            if self.time_emb_proj is not None:
-                temb = self.time_emb_proj(temb)
-            temb = temb[:, :, None, None, None]
-        
-        # if using default time embedding normalization, add the embedding directly
-        if temb is not None and self.time_embedding_norm == "default":
-            hidden_states = hidden_states + temb
-        
-        # apply second norm
-        hidden_states = self.norm2(hidden_states)
-
-        # if using scale_shift time embedding normalization, split embedding into scale and shift and apply them
-        if temb is not None and self.time_embedding_norm == "scale_shift":
-            scale, shift = torch.chunk(temb, 2, dim=1)
-            hidden_states = hidden_states * (1 + scale) + shift
-        
-        # apply non-linearity, dropout, and second convolution
-        hidden_states = self.nonlinearity_fn(hidden_states)
-        hidden_states = self.dropout_layer(hidden_states)
-        hidden_states = self.conv2(hidden_states)
-
-        # apply shortcut connection if defined
-        if self.conv_shortcut_layer is not None:
-            input_tensor = self.conv_shortcut_layer(input_tensor)
-
-        output_tensor = (input_tensor + hidden_states) / self.output_scale_factor
-
-        return output_tensor
-
-
-class Mish(nn.Module):
-    """
-    applies the mish activation function.
-    """
-    def forward(self, hidden_states):
-        return hidden_states * torch.tanh(nn.functional.softplus(hidden_states))
